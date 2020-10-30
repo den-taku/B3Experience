a230671579dabb0e3e7b741a0032ccdfd416b3e6
DenTaku <dentaku1412@gmail.com>
2020-10-30 15:31:14 +0900

    Add constants

--- file/octave/vain.m
+++ file/octave/vain.m
@@ -2,6 +2,12 @@ pkg load control;
 
 clear;
 
+R1=20e3;
+R2=300e3;
+K=1.03;
+TE=1.3267e-3;
+TM=0.0909;
+
 G=tf([15.45], [1.17e-4 9.13e-2 16.45]);
 stepsize=0.5;
 
--- file/octave/vain2.m
+++ file/octave/vain2.m
@@ -2,11 +2,18 @@ pkg load control;
 
 clear;
 
+R1=20e3;
+R2=300e3;
+K=1.03;
+TE=1.3267e-3;
+TM=0.0909;
+
+
 G=tf([15.45], [1.17e-4 9.13e-2 16.45]);
-stepsize=1.0;
+stepsize=0.5;
 
 [y_unit t]=step(G,1);
-y=(y_unit*stepsize)-0.5;
+y=y_unit*stepsize;
 u=ones(length(t),1)*stepsize;
 
 figure
