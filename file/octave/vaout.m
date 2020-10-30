424e9a57bdf71a7942162bd19dd4d0d4fcbb649d
DenTaku <dentaku1412@gmail.com>
2020-10-30 13:42:01 +0900

    Add vaout.m

---
+++ file/octave/vaout.m
@@ -0,0 +1,16 @@
+pkg load control;
+
+clear;
+
+G=tf([1.03], [1.17e-4 9.13e-2 16.45]);
+stepsize=0.5;
+
+[y_unit t]=step(G,1);
+y=y_unit*stepsize;
+u=ones(length(t),1)*stepsize;
+
+figure
+plot(t,u,'b--',t,y,'r-')
+legend('u','y');
+xlabel('Time/s');
+ylabel('Voltage/V');
