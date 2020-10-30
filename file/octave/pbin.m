db2d5c0896cf2be71c3a4726f620058c79a208bc
DenTaku <dentaku1412@gmail.com>
2020-10-30 14:08:33 +0900

    Add pbin.m

---
+++ file/octave/pbin.m
@@ -0,0 +1,16 @@
+pkg load control;
+
+clear;
+
+G=tf([7.836e-2 3.91818], [2.5125e-8 2.2925e-5 2.8245e-3 1.07e-1 3.91818]);
+stepsize=5.5;
+
+[y_unit t]=step(G,1);
+y=y_unit*stepsize;
+u=ones(length(t),1)*stepsize;
+
+figure
+plot(t,u,'b--',t,y,'r-')
+legend('u','y');
+xlabel('Time/s');
+ylabel('Voltage/V');
