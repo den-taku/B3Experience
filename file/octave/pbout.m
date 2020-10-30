b20bd6cb091289d20792c7cf4b1df46517122c95
DenTaku <dentaku1412@gmail.com>
2020-10-30 17:27:40 +0900

    Add csv data

--- file/octave/pbout.m
+++ file/octave/pbout.m
@@ -2,6 +2,8 @@ pkg load control;
 
 clear;
 
+A=csvread("10207out.csv");
+
 R1=470e3;
 R2=100e3;
 R3=2e3;
@@ -21,12 +23,13 @@ G=C/(1+C*P);
 %G=tf([TE*TM*C1*R2*R3*R4 R2*R4*(TE*TM+(TE+TM)*C1*R3) R2*R4*(TE+TM+C1*R3) R2*R4 0], [TE*TM*C2*R1*R3*R4 R1*R3*(TE*TM+(TE+TM)*C2*R4) R1*R3*(TE+TM+C2*R4) R3*(R1+K*C1*R2*R4) K*R2*R4]);
 stepsize=5.5;
 
-[y_unit t]=step(G,1);
+t=0:0.00001:0.5;
+[y_unit t]=step(G,t);
 y=y_unit*stepsize;
 u=ones(length(t),1)*stepsize;
 
 figure
-plot(t,u,'b--',t,y,'r-')
-legend('u','y');
+plot(t,u,'b--',t,y,'r-', A(:,1), A(:,2), 'b-', A(:,1), A(:,3), 'g-')
+legend('u','y', 'g');
 xlabel('Time/s');
 ylabel('Voltage/V');
