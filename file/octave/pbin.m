7591f4e5c7a53d789775a1532498b96c98251b4d
DenTaku <dentaku1412@gmail.com>
2020-10-30 17:05:15 +0900

    Modify all files

--- file/octave/pbin.m
+++ file/octave/pbin.m
@@ -6,15 +6,20 @@ R1=470e3;
 R2=100e3;
 R3=2e3;
 R4=7.5e3;
+C1=10e-6;
+C2=1e-6;
 K=1.03;
 TE=1.3267e-3;
 TM=0.0909;
 ke=0.0286479;
 kp=4.77;
 
+C=tf([C1*R3*R2*R4 R2*R4], [C2*R4*R1*R3 R1*R3]);
+P=tf([kp*K/ke], [TE*TM TE+TM 1 0]);
 
-G=tf([7.836e-2 3.91818], [2.5125e-8 2.2925e-5 2.8245e-3 1.07e-1 3.91818]);
-stepsize=5.5;
+G=C*P/(1+C*P);
+%G=tf([K*R2*R4*C1*R3 K*R2*R4], [TE*TM*C2*R1*R3*R4 R1*R3*(TE*TM+(TE+TM)*C2*R4) R1*R3*(TE+TM+C2*R4) R3*(R1+K*C1*R2*R4) K*R2*R4]);
+stepsize=5;
 
 [y_unit t]=step(G,1);
 y=y_unit*stepsize;
